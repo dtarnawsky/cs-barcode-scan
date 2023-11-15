eb270116ecae56bcb2405555b5ffa9e9d29c78a8
damian.ionic <damian@ionic.io>
2023-11-15 06:17:58 -0800

    Initial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeFormat, CameraPermissionState, GoogleBarcodeScannerModuleInstallProgressEvent, GoogleBarcodeScannerModuleInstallState } from '@capacitor-mlkit/barcode-scanning';
+import { PluginListenerHandle } from '@capacitor/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  isAlertOpen = false;
+  busy = false;
+  installHandle: PluginListenerHandle | undefined = undefined;
+  installing = false;
+
+  constructor() { }
+
+  public async onScan() {
+    try {
+      this.busy = true;
+      if (!await this.checkSettings()) {
+        this.busy = false;
+        return;
+      }
+      await BarcodeScanner.scan({
+        formats: [BarcodeFormat.QrCode, BarcodeFormat.Code128]
+      });
+    } catch (err) {
+
+      this.setOpen(true);
+    } finally {
+      this.busy = false;
+    }
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+
+  private async checkSettings(): Promise<boolean> {
+    const result = await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
+    if (!result.available) {
+      if (!this.installHandle) {
+        this.installHandle = BarcodeScanner.addListener('googleBarcodeScannerModuleInstallProgress', (event: GoogleBarcodeScannerModuleInstallProgressEvent) => {          
+          if (event.state == GoogleBarcodeScannerModuleInstallState.COMPLETED) {
+            this.installing = false;
+          }
+        });
+      }
+      this.installing = true;
+      await BarcodeScanner.installGoogleBarcodeScannerModule(); 
+      return false;     
+    }
+
+    const permitted = await BarcodeScanner.checkPermissions();
+    console.log(`Permissions is ${permitted.camera}`);
+
+    if (permitted.camera !== 'granted') {
+      const response = await BarcodeScanner.requestPermissions();
+      console.log(`Permission is ${response.camera}`);
+      return (response.camera == 'granted');
+    } else {
+      return true;
+    }
+    return false;
+  }
+
+}
